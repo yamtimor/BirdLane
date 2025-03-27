30774db6479f68fbdaba0e174ced9394fe82485a
yamtimor <yamtimor@gmail.com>
2025-03-27 21:53:50 +0200

    Initiate transform function

--- src/main/kotlin/birdlane/dsl/PipelineBuilder.kt
+++ src/main/kotlin/birdlane/dsl/PipelineBuilder.kt
@@ -4,4 +4,10 @@ class PipelineBuilder {
     fun extract(source: String) {
         println("Extracting from $source")
     }
+
+    fun transform(block: () -> Unit) {
+        println("Transforming...")
+        block()
+    }
+
 }
