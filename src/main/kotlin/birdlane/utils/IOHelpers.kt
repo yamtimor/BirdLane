48fd050d510ffd8e17121e81faa74092159e6f9f
yamtimor <yamtimor@gmail.com>
2025-07-15 01:26:07 +0300

    IOHelpers.kt with a readCsv function

--- src/main/kotlin/birdlane/utils/IOHelpers.kt
+++ src/main/kotlin/birdlane/utils/IOHelpers.kt
@@ -1,4 +1,13 @@
 package birdlane.utils
 
-class IOHelpers {
+import java.io.File
+
+fun readCsv(path: String): List<Map<String, String>> {
+    val lines = File(path).readLines()
+    val headers = lines.first().split(",")
+
+    return lines.drop(1).map { row ->
+        val values = row.split(",")
+        headers.zip(values).toMap()
+    }
 }
